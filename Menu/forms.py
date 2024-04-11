4bf4e159ef7ced0ccbe360b7168343b5d6d1d88a
Marco25005 <Bermudezmarco01@gmail.com>
2024-04-11 16:46:37 -0400

    autoguardado2

--- Menu/forms.py
+++ Menu/forms.py
@@ -1,9 +1,14 @@
-from django.forms import ModelForm
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from .models import Categoria
-
-# Crear la clase del formulario
-class CategoriaForm(ModelForm):
+class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ['entradas', 'postre', 'platoPrincipal']
 
+    def __init__(self, *args, **kwargs):
+        super(CategoriaForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Guardar'))
--- Restaurante/settings.py
+++ Restaurante/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'Menu',
 ]
 
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'Menu/Templates/'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
